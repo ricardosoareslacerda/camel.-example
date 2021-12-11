c0e50bd62811416d323ac414d90a3d24702a2206
ricardosoareslacerda <ricardosoareslacerda@gmail.com>
2021-12-11 12:14:49 -0300

    commit inicial

---
+++ src/test/java/br/com/fiap/scj/camel/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scj.camel.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
