7b10a14b5759ecabaf75c470375baa14d3351e5c
ricardosoareslacerda <ricardosoareslacerda@gmail.com>
2021-12-14 15:20:01 -0300

    Update MyBeanService.java
    Retirado o setName para pegar a informação do name no body da request post

--- src/main/java/br/com/fiap/scj/camel/example/services/MyBeanService.java
+++ src/main/java/br/com/fiap/scj/camel/example/services/MyBeanService.java
@@ -7,7 +7,6 @@ import java.util.Random;
 public class MyBeanService {
 
     public static void example(MyBean bean) {
-        bean.setName("Hello, meu primeiro método camel");
         bean.setId(new Random ().nextInt());
     }
 }
